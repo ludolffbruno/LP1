916b99ecf72f528a70d8ddf4cdcfb0ff234fa506
Bruno Ludolff <brunoludolff@outlook.com>
2019-04-19 15:48:30 -0300

    ..

--- Ex-4.1/Exe4.2.c
+++ Ex-4.1/Exe4.2.c
@@ -3,17 +3,19 @@
 int eh_primo (int n) {
 	int i;
 	int primo;
-	int div;	
-
+	int div=0;	
+	
 	for(i=1; i<=n; i++) {
 		if (n%i == 0) {
  			primo = 0; // nao e primo
 			div += 1;
+			//printf("%d \n",div);
+
 		}
 		else (n%i != 0); {
 			primo = 1;
 		}
-	}	
+	}
 	if (div == 2)
 		return 1; //  é 
 	else (div != 2);
@@ -22,22 +24,24 @@ int eh_primo (int n) {
 int main()
 {
 	int max,p,N;
-
+	
 	printf("digite o MAX: ");
 	scanf("%d",&max);
 
+	printf("O numero 1 é Primo! \n "); // 1 é primo.
+	
 	for(N=1; N<=max; N++) {
 		p = eh_primo(N);
-		printf("%d\n",p);
-/*
+//		printf("%d\n",p);
+
 		if (p == 1) {
-			printf("O numero %d é Primo! \n",i);
+			printf("O numero %d é Primo! \n",N);
 		}
 		else {
 			if (p == 0) {
-			printf("O numero %d NÃO é Primo! \n",i);
+			//printf("O numero %d NÃO é Primo! \n",N);
 			}	
-		} */ 
+		} 
 	}
 
 	return 0;
