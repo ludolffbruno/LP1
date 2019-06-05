67af9f169a576f682c07eaf268c2084606fa8ce6
Bruno Ludolff <brunoludolff@outlook.com>
2019-06-04 23:29:46 -0300

    Feito

--- Ex-6.1/Ex6.5.c
+++ Ex-6.1/Ex6.5.c
@@ -9,14 +9,13 @@ int strlen2(char* str){
 	return count;	// retorna o tamanho da string
 }
 
-void strjoin2(char* d; char* o1; char* o2){
-	int n = 0;
-	int i = 0;
-	int j = 0;
+void strjoin2(char* d, char* o1, char* o2){
+	int i ;
+	int j ;
 	
 	if (d[0] != '\0'){  // preparando o vetor.
  		d[0] = '\0';
-
+	}
 	for(i=0; i<strlen2(o1); i++){ 
 		d[i] = o1[i];
 	}
@@ -24,21 +23,22 @@ void strjoin2(char* d; char* o1; char* o2){
 		d[i] = o2[j];
 		i++;
 	}
-
+	
 }
 
 int main() {
-	char p1[50];
-	char p2[50];
-	char destino[100];
+	char o1[50];
+	char o2[50];
+	char d[100];
 
 	printf("Digite as Strings: ");
-	scanf("%s",p1); 
-	scanf("%s",p2);
+	scanf("%s",o1); 
+	scanf("%s",o2);
 
-	printf("%s\n %s\n",p1,p2);
+//	printf("%s\n%s\n",o1,o2);
 
-	strjoin2(destino, p1, p2);
+	strjoin2(d, o1,o2);
+	printf("%s\n",d);
 
    return 0; 
 }
