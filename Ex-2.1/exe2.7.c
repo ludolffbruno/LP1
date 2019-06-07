21821221f4c7171f6d03edb4242dbc4ac020f3b9
Bruno Ludolff <brunoludolff@outlook.com>
2019-06-07 00:44:20 -0300

    Concluido

--- Ex-2.1/exe2.7.c
+++ Ex-2.1/exe2.7.c
@@ -1,66 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
-// Exercicio #2.7 
-
 /* 
 O Campeonato Brasileiro está na reta final com 20 times na disputa.
-
 Uma vitória vale 3 pontos, um empate vale 1 ponto e uma derrota vale 0 pontos. Escreva um programa que, para cada time, leia o nome, número de vitórias, número de empates e número de derrotas.
-
 Ao final, o programa deve escrever o nome dos times com mais e menos pontos até o momento.*/
 
-int main() {
+void copiarString(char *copiarDaqui, char *colarAqui){
+	while (*copiarDaqui != '\0') {
+		*colarAqui = *copiarDaqui;
+		++copiarDaqui;
+		++colarAqui;
+	}
+	*colarAqui = '\0';
+}
 
-    int  i=0 , vit , emp , der , pontuacao , maiorP = 0 , menorP = 999  ;
+int main(void) {
+    int  i=0, vit=0, emp=0, der=0, pontuacao=0, maiorP=0, menorP=9999;
     char nome[100], salvar_maior[100] , salvar_menor[100];
 
-    for (i ; i=4 ; i++) {
-		// vit , emp , der = 0; 
+    for (i=0 ; i<21 ; i++) {
+		//vit , emp , der = 0; 
 
         printf("Nome do time: \n");
         scanf("%s",nome);   // %c --> Um Caractere ; %s --> Uma Sequência.
 						   // Não colocar indice na variavel, nao precisa &
+//ok	printf("%s\n",nome);
         printf("Vitorias: \n");
         scanf("%d",&vit);
 
         printf("Empate: \n");
         scanf("%d",&emp);
 
-        printf("Derrotas: \n");
+		printf("Derrotas: \n");
         scanf("%d",&der);
-		
+
 		pontuacao = (vit*3 + emp + der*0);
+//ok	printf("Pontuacao: %d\n",pontuacao);		
 
 		if (pontuacao > maiorP) {
 			maiorP = pontuacao;	
-			salvar_maior[i] = nome[i];  // sem indice?	
-		} else {
-			menorP = pontuacao;
-			salvar_menor[i] = nome[i];  // sem indice
+			copiarString(nome,salvar_maior);
+//ok		printf("%s\n",salvar_maior);
 		}
-		//printf("Vencedor: %s\n",salvar_maior);
-		//printf("Perdedor: %s\n",salvar_menor);		
-// PROGRAMA ESTA EM LOOP NAO SEI PORQUE :\ 
+		if (pontuacao <= menorP) {
+			menorP = pontuacao;
+			copiarString(nome,salvar_menor);
+		}
+		printf("Vencedor: %s\n",salvar_maior);
+		printf("Perdedor: %s\n",salvar_menor);	
+
+//		printf("valor de i: %d\n",i);	
     }
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
